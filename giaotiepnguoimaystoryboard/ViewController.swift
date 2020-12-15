b2c2010d225df6254b336efe1d4701073947a6ca
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-12-15 11:44:37 +0700

    Initial Commit

---
+++ giaotiepnguoimaystoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  giaotiepnguoimaystoryboard
+//
+//  Created by MM on 15/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
